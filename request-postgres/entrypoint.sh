5a2acf6a6e3fc87a8d9c2c381b97932d0ed8fe7d
Neil Duffy <skivy71@gmail.com>
2024-01-22 01:11:02 +0000

    almost

--- request-postgres/entrypoint.sh
+++ request-postgres/entrypoint.sh
@@ -1,14 +1,69 @@
 #!/bin/bash
 
-italic() {
+echo_italic() {
     echo -e "\e[3m${1}\e[0m"
 }
 
-sql() {
+env_check() {
+    local envs=(
+        PG_USER
+        PG_PASS
+        PG_HOST
+        PG_PORT
+        DOCKER_USER
+        DOCKER_PASS
+        DOCKER_DB
+    )
+    for env in "${envs[@]}"; do
+        [[ -n ${!env} ]] || error "Empty $env ENV!"
+    done
+}
+
+
+error() {
+    echo "$1"
+    exit 1
+}
+
+sql_db_exists() {
+    local db=$1
+    [[ $(sql_exists pg_database datname "$db") -eq 1 ]] && echo 1
+}
+
+sql_database() {
+    local db=$1
+    local user=$2
+    [[ $(sql_db_exists "$db") -eq 1 ]] || {
+        sql_query "CREATE DATABASE ${db}"
+        sql_query "GRANT ALL PRIVILEGES ON DATABASE ${db} TO ${user}"
+    }
+}
+
+sql_drop() {
+    local db=$1
+    [[ $(sql_db_exists "$db") ]] && {
+        echo "dropdb: $1..."
+        dropdb \
+            -U "$PG_USER" \
+            -h "$PG_HOST" \
+            -p "$PG_PORT" \
+            "$db"
+
+        }
+}
+
+sql_exists() {
+    local db=$1
+    local col=$2
+    local val=$3
+    sql_query "SELECT 1 FROM ${db} WHERE ${col}='${val}'" | tail -n +4 | grep -q 1 && echo 1
+}
+
+sql_query() {
     local query=$1
     local db="postgres"
     echo -n "Running SQL: "
-    italic "$query"
+    echo_italic "$query"
     psql \
         -q \
         -U "$PG_USER" \
@@ -18,29 +73,13 @@ sql() {
         -c "$query"
 }
 
-exists() {
-    local db=$1
-    local col=$2
-    local val=$3
-    sql "SELECT 1 FROM ${db} WHERE ${col}='${val}'" | tail -n +4 | grep -q 1 && echo 1
-}
-
-user() {
+sql_user() {
     local user=$1
-    echo
-    echo "User, name: ${user}..."
-    [[ $(exists pg_roles rolname "$user") -eq 1 ]] || sql "CREATE USER $user"
+    [[ $(sql_exists pg_roles rolname "$user") -eq 1 ]] || sql_query "CREATE USER $user"
 }
 
-database() {
-    local db=$1
-    local user=$2
-    echo
-    echo "Database, name: ${db}, user: ${user}..."
-    [[ $(exists pg_database datname "$db") -eq 1 ]] || {
-        sql "CREATE DATABASE ${db}"
-        sql "GRANT ALL PRIVILEGES ON DATABASE ${db} TO ${user}"
-    }
+complete() {
+    
 }
 
 # main
@@ -49,5 +88,10 @@ echo "connection: postgres://${PG_USER}:<PG_PASS>@${PG_HOST}:${PG_PORT}/postgres
 
 export PGPASSWORD=$PG_PASS
 
-user "$DOCKER_USER" \
-    && database "$DOCKER_DB" "$DOCKER_USER"
+if [[ $PG_DROP -eq 1 ]]; then
+    sql_drop "$DOCKER_DB"
+    exit
+else
+    sql_user "$DOCKER_USER" \
+        && sql_database "$DOCKER_DB" "$DOCKER_USER"
+fi
